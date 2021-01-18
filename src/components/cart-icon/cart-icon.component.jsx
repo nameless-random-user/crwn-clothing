c9ea207bc4ffd7cbf8b814867ca01965916018f9
namless-random-user <rockisdead4444@gmail.com>
2021-01-18 10:45:54 +0100

    added checkout page, checkout-item component, and updated cart to use selectors from reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { createStructuredSelector } from "reselect";
+
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
@@ -16,8 +18,8 @@ function CartIcon({toggleCartHidden, itemCount}){
   )
 }
 
-const mapStateToProps = (state) => ({
-  itemCount : selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount : selectCartItemsCount
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -9,10 +9,23 @@ export const selectCartItems = createSelector(
   cart => cart.cartItems
 )
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+)
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => 
     cartItems.reduce((accumulatedQuantity, cartItem) => {
       return accumulatedQuantity + cartItem.quantity;
+    }, 0)
+)
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce((accumulated, cartItem) => {
+      return accumulated +  cartItem.quantity * cartItem.price
     }, 0)
 )
